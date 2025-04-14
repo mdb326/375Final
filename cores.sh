6fdb065c90716990cdf4cda4e0e0243314da5f64
CPU Scaling Team <mdb326@lehigh.edu>
2025-04-14 17:58:38 -0400

    Make script to adjust some of teh cores frequency

---
+++ cores.sh
@@ -0,0 +1,27 @@
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu8/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu9/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu10/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu11/cpufreq/scaling_max_freq
+# echo 1800000 | sudo tee /sys/devices/system/cpu/cpu12/cpufreq/scaling_max_freq
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu8/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu9/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu10/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu11/cpufreq/scaling_max_freq
+echo 5300000 | sudo tee /sys/devices/system/cpu/cpu12/cpufreq/scaling_max_freq
