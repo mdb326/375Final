c2e2d3b54e2d6768ad12906dad1c6bd0e35d56dd
CPU Scaling Team <mdb326@lehigh.edu>
2025-04-17 13:23:36 -0400

    A few tests with cores.sh

--- cores.sh
+++ cores.sh
@@ -27,16 +27,16 @@
 # echo 4200000 | sudo tee /sys/devices/system/cpu/cpu17/cpufreq/scaling_max_freq
 # echo 4200000 | sudo tee /sys/devices/system/cpu/cpu18/cpufreq/scaling_max_freq
 
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu17/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu18/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu19/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu20/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu21/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu22/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu23/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu24/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu25/cpufreq/scaling_max_freq
-echo 4200000 | sudo tee /sys/devices/system/cpu/cpu26/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu17/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu18/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu19/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu20/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu21/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu22/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu23/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu24/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu25/cpufreq/scaling_max_freq
+echo 2200000 | sudo tee /sys/devices/system/cpu/cpu26/cpufreq/scaling_max_freq
 
 
 cat /sys/devices/system/cpu/cpu15/cpufreq/scaling_max_freq
