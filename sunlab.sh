343e91bbbd9d2ad28b56486956c524c41e6be29b
CPU Scaling Team <mdb326@lehigh.edu>
2025-05-09 14:14:07 -0400

    update script

--- sunlab.sh
+++ sunlab.sh
@@ -1,7 +1,10 @@
+git stash
+git pull
+
 rm test2
 # rm bank
 g++ -std=c++17 -o test2 test.cpp -g -pthread -O3
 # sh cores.sh
 # g++ -std=c++17 -o bank bank.cpp -pthread -O3
-./test2
+# ./test2
 # sudo ./bank
